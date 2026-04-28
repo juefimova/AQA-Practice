305cde13715de3cc6fceb9602e3f9edf9a9014e3
Yuliya Yafimava <ju.efimova7@gmail.com>
2026-04-28 18:53:53 +0200

    Task 3

--- api.test.js
+++ api.test.js
@@ -14,4 +14,20 @@ const getUser = async () => {
     }
 }
 
-getUser();
+await getUser();
+
+
+console.log('Task 3');
+async function getBrokenData() {
+    try {
+        const response = await fetch('https://reqres.in/api/users/23/сломанный_путь');
+        //отвечает json с просьбой добавить ключ
+        //если с 'https://www.google.com/404', то все ок
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(`Тест не упал, но мы поймали ошибку: ${error.message}`);
+    }
+
+}
+await getBrokenData();
--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@ export default function checkResponse(response) {
     if (response.ok) {
         return true;
     } else {
-        console.error(`Статус: ${response.status}`); //проверка статуса
+        console.log(`Статус: ${response.status}`); //проверка статуса
         return false;
     }
 
