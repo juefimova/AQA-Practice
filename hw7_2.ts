f1c6231e75677943ab8b3d8e5ddf959b115432d4
Yuliya Yafimava <ju.efimova7@gmail.com>
2026-04-29 17:24:40 +0200

    Task 2

---
+++ hw7_2.ts
@@ -0,0 +1,21 @@
+interface UserData {
+    id: number,
+    email: string,
+    tags: string[]
+}
+
+interface ApiResponse {
+    status: string,
+    data: UserData
+}
+
+const mockResponse: ApiResponse = {
+    status: "success",
+    data: {
+        id: 101,
+        email: "test@qa.com",
+        tags: ["e2e", "smoke"]
+    }
+}
+
+console.log(mockResponse);
