58b6028f296e82a474bc277089aa48e565ee8595
Yuliya Yafimava <151470525+juefimova@users.noreply.github.com>
2026-05-24 14:20:42 +0200

    Delete hw9 directory

--- hw9/checkbox-task.spec.js
+++
@@ -1,7 +0,0 @@
-import {test, expect} from "@playwright/test";
-test('Succesful user login', async({page}) => {
-    await page.goto('https://demoqa.com/checkbox');
-    await page.locator('.rc-tree-switcher.rc-tree-switcher_close').click();
-    await page.getByLabel('Select Desktop').check();
-    await expect(page.locator('.text-success')).toContainText(['desktop','notes','commands']);
-})
